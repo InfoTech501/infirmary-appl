990ae3288ae2a6b3ba71784711f22a1351e534dd
jakeeviado <jakematthewviado204@gmail.com>
2025-05-01 21:29:49 +0800

    initial project commit

---
+++ src/main/java/com/rocs/infirmary/application/InfirmaryApplApplication.java
@@ -0,0 +1,13 @@
+package com.rocs.infirmary.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfirmaryApplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfirmaryApplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocs/infirmary/application/InfirmaryApplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocs.infirmary.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfirmaryApplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
