9217ff2f397fca8614186f578bb12b09c17ee204
jakeeviado <jakematthewviado204@gmail.com>
2025-09-16 18:14:06 +0800

    added missing docs

--- src/main/java/com/rocs/infirmary/application/domain/medicalRecord/MonthlyAilmentReport/MonthlyAilmentReport.java
+++ src/main/java/com/rocs/infirmary/application/domain/medicalRecord/MonthlyAilmentReport/MonthlyAilmentReport.java
@@ -2,6 +2,9 @@ package com.rocs.infirmary.application.domain.medicalRecord.MonthlyAilmentReport
 
 import lombok.Data;
 
+/**
+ * {@code MonthlyAilmentReport} represents a template for generating monthly common ailments report.
+ * */
 @Data
 public class MonthlyAilmentReport {
     private Integer rank;
