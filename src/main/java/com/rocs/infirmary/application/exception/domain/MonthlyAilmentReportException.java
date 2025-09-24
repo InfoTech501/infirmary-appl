c69dab9c0def67617fae6ff4ab5c65bd975c26f3
jakeeviado <jakematthewviado204@gmail.com>
2025-09-24 19:44:43 +0800

    added missing docu

--- src/main/java/com/rocs/infirmary/application/exception/domain/MonthlyAilmentReportException.java
+++ src/main/java/com/rocs/infirmary/application/exception/domain/MonthlyAilmentReportException.java
@@ -1,6 +1,14 @@
 package com.rocs.infirmary.application.exception.domain;
 
+/**
+ * Thrown exceptions when error occurs during monthly ailment report operations.
+ */
 public class MonthlyAilmentReportException extends RuntimeException {
+  /**
+   * Constructs an exception with the specified message and cause.
+   * @param message the detail message
+   * @param cause the cause of this exception
+   */
   public MonthlyAilmentReportException(String message, Throwable cause) {
     super(message, cause);
   }
