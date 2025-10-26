76dda2bbd877037c46a92ab650f81e16f59ac3db
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-10-26 12:05:34 +0800

    added documentation for exceptions

--- src/main/java/com/rocs/infirmary/application/exception/domain/DepartmentNotFoundException.java
+++ src/main/java/com/rocs/infirmary/application/exception/domain/DepartmentNotFoundException.java
@@ -1,6 +1,13 @@
 package com.rocs.infirmary.application.exception.domain;
-
+/**
+ * this handles the exception for department not found
+ * */
 public class DepartmentNotFoundException extends RuntimeException {
+    /**
+     * this creates a constructor for {@code DepartmentNotFoundException}
+     *
+     * @param message contains the message about the exception
+     * */
     public DepartmentNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/rocs/infirmary/application/exception/domain/InvalidCredentialException.java
+++ src/main/java/com/rocs/infirmary/application/exception/domain/InvalidCredentialException.java
@@ -1,6 +1,13 @@
 package com.rocs.infirmary.application.exception.domain;
-
+/**
+ * this handles the exception for invalid credentials
+ * */
 public class InvalidCredentialException extends RuntimeException {
+    /**
+     * this creates a constructor for {@code InvalidCredentialException}
+     *
+     * @param message contains the message about the exception
+     * */
     public InvalidCredentialException(String message) {
         super(message);
     }
